b5c39b26fb97be282a358758178f6d269c4ec088
nirav5811 <niravnamlee@gmail.com>
2023-03-15 21:35:02 +0000

    second push on git hub and commit again

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello this is first trail of git hub!");
+        System.out.printf("fist file second push to github");
+    }
+}
